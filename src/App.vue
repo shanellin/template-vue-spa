4ea8ffd08ba09fa7ea113dc2beea4fef647afcd6
Shane Lin <yushanelin@redso.com.hk>
2021-04-25 15:45:27 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="App">
+    <div class="hello">Hello {{ who }}</div>
+    <router-link to="/">首頁</router-link> <br>
+    <router-link to="/page2">page2</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      who: "Vue and Webpack !!",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.hello {
+  padding: 0.5em;
+  font-size: 2em;
+  background-color: #fcf;
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue';
+import router from './router/router.js';
+
+new Vue({
+  el: '#app',
+  mounted: function () {
+    console.log('Hello Webpack and Vue !');
+  },
+  router,
+  components: { App },
+  template: '<App/>'
+});
